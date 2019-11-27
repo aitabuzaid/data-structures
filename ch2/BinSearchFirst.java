78f81d3bb230e4f4f43bb5348bd5a628e448d1fc
aitabuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2019-11-27 18:13:53 +0300

    Some bug fixes for binary search with multiple elements

--- ch2/BinSearchFirst.java
+++ ch2/BinSearchFirst.java
@@ -1,24 +1,38 @@
 package ch2;
+/**
+ * A binary search function that returns
+ * the index of the first occurrence of
+ * an element in an array
+ * @author Abdulrahman
+ *
+ */
 
 public class BinSearchFirst {
 	public static void main(String args[]) {
 		int[] A = new int[20];
 		for (int i = 0; i < A.length; i++) {
 			A[i] = i/4;
-			System.out.println(i+" : "+A[i]);
 		}
+		
+		// test cases
+		System.out.println(find(A, -1));
+		System.out.println(find(A, 0));
+		System.out.println(find(A, 1));
 		System.out.println(find(A, 2));
+		System.out.println(find(A, 3));
+		System.out.println(find(A, 4));
+		System.out.println(find(A, 5));
 	}
 	public static int find(int[] A, int K) { 
 		int low = 0;
 		int hi = A.length-1;
 		int mid;
 		while (low <= hi) {
-			mid = (hi-low)/2;
+			mid = (hi+low)/2;
 			if (A[mid] > K)
-				hi = mid;
+				hi = mid-1;
 			else if (A[mid] < K)
-				low = mid;
+				low = mid+1;
 			else {
 				if (mid == 0) {
 					return mid;
@@ -27,7 +41,7 @@ public class BinSearchFirst {
 					return mid;
 				}
 				else
-					hi = mid;
+					hi = mid-1;
 			}
 		}
 		return -1;
