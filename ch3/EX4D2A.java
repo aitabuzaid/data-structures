ba2c79b53c2ddf57884b004299e1f2f2a0b5686e
aitabuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2019-11-29 14:20:21 +0300

    List ADT exercise 4.2a

---
+++ ch3/EX4D2A.java
@@ -0,0 +1,18 @@
+package ch3;
+
+public class EX4D2A {
+
+	public static void main(String[] args) {
+		
+		// setting up the exercise conditions
+		List<Integer> list = new AList<Integer>();
+		System.out.println(list);
+		list.append(2);
+		System.out.println(list);
+		list.append(23);
+		System.out.println(list);
+		list.append(15);
+		System.out.println(list);
+	}
+
+}
