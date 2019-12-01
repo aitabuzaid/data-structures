8ceb4dab938f620c3781d514d73f66d0623f517b
aitabuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2019-12-01 21:41:16 +0300

    Added linked list code and started EX4.5

---
+++ ch3/EX4D5.java
@@ -0,0 +1,11 @@
+package ch3;
+
+public class EX4D5 {
+
+	public static void main(String[] args) {
+		
+		// setting up the exercise conditions
+		List<Integer> list = new LList<Integer>(20);
+		
+	}
+}
