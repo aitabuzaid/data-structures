614520dc2ba134df66a44407d35a34d421840432
aitabuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2019-12-06 13:42:03 +0300

    Adding a skeleton for static reverse method

--- ch3/EX4D18.java
+++ ch3/EX4D18.java
@@ -3,12 +3,17 @@ import java.util.*;
 
 // Function to reverse order 
 // of Queue elements using a stack
+
 public class EX4D18 {
 	
 
 	public static void main(String args[]) {
 		Stack<Integer> stack = new Stack<Integer>();
 		Queue<Integer> queue = new LinkedList<Integer>();	
-		
+		EX4D18.reverse(queue, stack);
+	}
+	
+	public static Queue<Integer> reverse(Queue<Integer> queue, Stack<Integer> stack) {
+		return queue;
 	}
 }
