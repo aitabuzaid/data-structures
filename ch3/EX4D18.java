0456822979dac2c0b89144e10c3dca96d3f53a2f
aitabuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2019-12-06 13:34:28 +0300

    Initial commit for Ex4.18

---
+++ ch3/EX4D18.java
@@ -0,0 +1,14 @@
+package ch3;
+import java.util.*;
+
+// Function to reverse order 
+// of Queue elements using a stack
+public class EX4D18 {
+	
+
+	public static void main(String args[]) {
+		Stack<Integer> stack = new Stack<Integer>();
+		Queue<Integer> queue = new LinkedList<Integer>();	
+		
+	}
+}
