7793ddf85f9c118d9d3bd805b1578820f4e8c449
aitabuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2019-12-07 22:50:24 +0300

    Initial commit for math oper. using linked lists

---
+++ ch3/P4D3.java
@@ -0,0 +1,22 @@
+package ch3;
+import java.util.*;
+
+// Function to reverse order 
+// of Queue elements using a stack
+
+public class P4D3 {
+	
+
+	public static void main(String args[]) {
+		LinkedList<Integer> int1 = new LinkedList<Integer>();
+		LinkedList<Integer> int2 = new LinkedList<Integer>();
+		
+		for (int i = 0; i < 10; i++) {
+			
+		}
+	}
+	
+	public static LinkedList<Integer> add(LinkedList<Integer> int1, LinkedList<Integer> int2) {
+		return int1;
+	}
+}
