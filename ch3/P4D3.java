27bac0cc701f5ce415519a547efa98198a061afd
aitabuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2019-12-07 22:57:40 +0300

    completed test code for add oper. using linked lists

--- ch3/P4D3.java
+++ ch3/P4D3.java
@@ -8,15 +8,29 @@ public class P4D3 {
 	
 
 	public static void main(String args[]) {
-		LinkedList<Integer> int1 = new LinkedList<Integer>();
-		LinkedList<Integer> int2 = new LinkedList<Integer>();
+		LinkedList<Integer> intList1 = new LinkedList<Integer>();
+		LinkedList<Integer> intList2 = new LinkedList<Integer>();
 		
-		for (int i = 0; i < 10; i++) {
-			
+		for (int i = 0; i < 30; i++) {
+			int int1 = (int)(10*Math.random());
+			int int2 = (int)(10*Math.random());
+			intList1.add(int1);
+			intList2.add(int2);
 		}
+		
+		System.out.println(intList1);
+		System.out.println(intList2);
+		System.out.println(add(intList1, intList2));
 	}
 	
-	public static LinkedList<Integer> add(LinkedList<Integer> int1, LinkedList<Integer> int2) {
-		return int1;
+	
+	
+	public static LinkedList<Integer> add(LinkedList<Integer> intList1, LinkedList<Integer> intList2) {
+		LinkedList<Integer> resList = new LinkedList<Integer>();
+		
+		while (!intList1.isEmpty() || !intList2.isEmpty()) {
+			
+		}
+		return resList;
 	}
 }
