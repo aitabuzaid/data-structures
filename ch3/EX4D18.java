bf7d9441d4c3cb589bbe72562e447b71a72b1508
aitabuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2019-12-07 22:42:01 +0300

    Completed reverse method of queue using a stack

--- ch3/EX4D18.java
+++ ch3/EX4D18.java
@@ -9,11 +9,23 @@ public class EX4D18 {
 
 	public static void main(String args[]) {
 		Stack<Integer> stack = new Stack<Integer>();
-		Queue<Integer> queue = new LinkedList<Integer>();	
+		Queue<Integer> queue = new LinkedList<Integer>();
+		
+		for (int i = 0; i < 20; i++)
+			queue.add(i);
+		
+		System.out.println(queue);
 		EX4D18.reverse(queue, stack);
+		System.out.println(queue);
 	}
 	
 	public static Queue<Integer> reverse(Queue<Integer> queue, Stack<Integer> stack) {
+		while (!queue.isEmpty()) {
+			stack.add(queue.remove());
+		}
+		while (!stack.isEmpty()) {
+			queue.add(stack.pop());
+		}
 		return queue;
 	}
 }
