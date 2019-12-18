7a7a193a6879576bfe902132a813d108d4ac5af1
aitabuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2019-12-18 22:15:37 +0300

    Added a funtion to do check if 2 gen trees are the same

--- ch6/GTNode.java
+++ ch6/GTNode.java
@@ -26,4 +26,22 @@ public interface GTNode {
 		}
 		printNode(rt);
 	}
+	
+	static boolean GTComp(GTNode rt1, GTNode rt2) {
+        boolean result = true;
+        if (!rt1.isLeaf() && rt2.isLeaf()) return false;
+        else if (rt1.isLeaf() && !rt2.isLeaf()) return false;
+        else if (rt1.isLeaf() && rt2.isLeaf()) return rt1.value() == rt2.value();
+        else {
+            GTNode temp1 = rt1.leftmostChild();
+            GTNode temp2 = rt2.leftmostChild();
+            while (temp1 != null && temp2 != null) {
+                result &= GTComp(temp1, temp2);
+                temp1 = temp1.rightSibling();
+                temp2 = temp2.rightSibling();
+            }
+            if ((temp1 == null && temp2 != null) || (temp1 != null && temp2 == null)) return false;
+        }
+        return result;
+    }
 }
