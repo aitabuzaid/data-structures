6850f1b9ba0a71cfd044241d908181aaa00d4fa4
aitabuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2019-12-19 20:44:39 +0300

    Implemented the GTNode class

--- ch6/GTNode.java
+++ ch6/GTNode.java
@@ -1,19 +1,50 @@
 package ch6;
 
 //General tree node ADT
-public interface GTNode {
-	public Object value();
-	public boolean isLeaf();
-	public GTNode parent();
-	public GTNode leftmostChild();
-	public GTNode rightSibling();
-	public void setValue(Object value);
-	public void setParent(GTNode par);
-	public void insertFirst(GTNode n);
-	public void insertNext(GTNode n);
-	public void removeFirst();
-	public void removeNext();
-	public static void printNode(GTNode n){};
+// General tree node ADT
+
+public class GTNode {
+    private Object value;
+    GTNode leftmostChild, rightSibling;
+    GTNode(Object value) {
+        this.value = value;
+    } 
+
+    GTNode(Object value, GTNode left, GTNode right) {
+        this.value = value;
+        this.leftmostChild = left;
+        this.rightSibling = right;
+    }
+
+    public Object value() {
+        return this.value;
+    }
+
+    public boolean isLeaf() {
+        return this.leftmostChild == null;
+    }
+
+    //public GTNode parent();
+    public GTNode leftmostChild() {
+        return this.leftmostChild;
+    }
+
+    public GTNode rightSibling() {
+        return this.rightSibling;
+    }
+    
+    public void setValue(Object value) {
+        this.value = value;
+    }
+    //public void setParent(GTNode par);
+
+    //public void insertFirst(GTNode n);
+
+    //public void insertNext(GTNode n);
+
+    //public void removeFirst();
+
+    //public void removeNext();
 
 
 	static void postorder(GTNode rt) {
