057b4f674bf5069c9801a45dd3b52b469488c512
aitabuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2019-12-25 09:09:56 +0300

    Added test case for quicksort

--- ch6/QuickSort.java
+++ ch6/QuickSort.java
@@ -2,9 +2,21 @@ package ch6;
 
 public class QuickSort {
 	public static void main(String[] args) {
-		System.out.print("Hi");
+		Integer[] arr = new Integer[20];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = (Integer)(int)(Math.random()*20);
+			System.out.print(arr[i]+ " ");
+		}
+		System.out.println();
+		quicksort(arr);
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i]+ " ");
+		}
 	}
 	
+	static void quicksort(Comparable[] arr) {
+		quicksort(arr, 0, arr.length-1);
+	}
 	static void quicksort(Comparable[] arr, int min, int max) {
 		int pivotIndex = (min+max)/2;
 		swap(arr, pivotIndex, max);
@@ -24,7 +36,8 @@ public class QuickSort {
 		int pivotIndex = right--;
 		while (left <= right) {
 			while(arr[left].compareTo(arr[pivotIndex]) < 0) left++;
-			while((arr[right].compareTo(arr[pivotIndex]) >= 0) && (right >= left)) right--;
+			System.out.println(left+" "+right);
+			while((right >= left) && (arr[right].compareTo(arr[pivotIndex]) >= 0)) right--;
 			if (right > left) swap(arr, left, right);
 		}
 		return left;
