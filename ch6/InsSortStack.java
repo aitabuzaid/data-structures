9db718447980c399a3e8883452671890a65664db
aitabuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2019-12-28 23:01:57 +0300

    Added method for insertion sort using stacks

--- ch6/InsSortStack.java
+++ ch6/InsSortStack.java
@@ -1,4 +1,5 @@
 package ch6;
+import java.util.*;
 
 public class InsSortStack {
 
@@ -6,5 +7,22 @@ public class InsSortStack {
 		// TODO Auto-generated method stub
 
 	}
+
+	public static Stack<Comparable> sort(Stack<Comparable> inStack){
+		Stack<Comparable> tempStack = new Stack<Comparable>();
+		Stack<Comparable> outStack = new Stack<Comparable>();
+		Comparable tempItem;
+		while (!inStack.isEmpty()) {
+			tempItem = inStack.pop();
+			while (!outStack.isEmpty() && tempItem.compareTo(outStack.peek()) > 0) {
+				tempStack.push(outStack.pop());
+			}
+			outStack.push(tempItem);
+			while (!tempStack.isEmpty()) outStack.push(tempStack.pop());
+		}
+
+	}
+	return outStack;
+}
 
 }
