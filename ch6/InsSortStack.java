602ff9649d3c3ded339a7d7109326937fe915311
aitabuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2019-12-29 19:04:16 +0300

    Added test case for insertion sort using stacks

--- ch6/InsSortStack.java
+++ ch6/InsSortStack.java
@@ -5,9 +5,13 @@ public class InsSortStack {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Stack<Comparable> stack = new Stack<Comparable>();
+		for (int i = 0; i < 20; i++) {
+			stack.push((int)(Math.random()*50));
+		}
+		System.out.println(stack);
+		System.out.println(InsSortStack.sort(stack));
 	}
-
 	public static Stack<Comparable> sort(Stack<Comparable> inStack){
 		Stack<Comparable> tempStack = new Stack<Comparable>();
 		Stack<Comparable> outStack = new Stack<Comparable>();
@@ -21,8 +25,8 @@ public class InsSortStack {
 			while (!tempStack.isEmpty()) outStack.push(tempStack.pop());
 		}
 
+
+		return outStack;
 	}
-	return outStack;
-}
 
 }
