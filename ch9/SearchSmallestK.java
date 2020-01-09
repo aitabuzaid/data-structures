b0e5e8b7a9b249ad0f170940a1b34690dbe0e13a
aitabuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2020-01-09 21:49:33 +0300

    Completed method to find the smallest kth value in an unsorted array

--- ch9/SearchSmallestK.java
+++ ch9/SearchSmallestK.java
@@ -1,44 +1,43 @@
 package ch9;
 
 /**
- * A new class for the Quicksort algorithm that
- * only sorts the first k values of an array of
- * size n, the algorithms checks the minimum and 
- * the partition values for the subarray before it
- * calls itself recursively, this ensures that
- * the algorithm does not do additional processing
- * once the first k values are sorted
+ * 
  * @author Abdulrahman
  *
  */
 public class SearchSmallestK {
 	public static void main(String[] args) {
 		Integer[] arr = new Integer[20];
-		int k = 4;
+		int k = 5;
 		System.out.println("The original array is:");
 		for (int i = 0; i < arr.length; i++) {
 			arr[i] = (Integer)(int)(Math.random()*200);
 			System.out.print(arr[i]+ " ");
 		}
 		
-		System.out.println("\nThe sorted array for the first "+
-							k+" elements is:");
-		quicksort(arr, k);
+		System.out.println("\nThe smallest k = "+
+							k+" element is: "+findSmallestK(arr, k));
+		
+		System.out.println();
+		quicksort(arr);
 		for (int i = 0; i < arr.length; i++) {
 			System.out.print(arr[i]+ " ");
 		}
+		
 	}
 	
-	static void quicksort(Comparable[] arr, int k) {
-		quicksort(arr, 0, arr.length-1, k);
+	static Comparable findSmallestK(Comparable[] arr, int k) {
+		return findSmallestK(arr, 0, arr.length-1, k);
 	}
-	static void quicksort(Comparable[] arr, int min, int max, int k) {
+	static Comparable findSmallestK(Comparable[] arr, int min, int max, int k) {
 		int pivotIndex = (min+max)/2;
 		swap(arr, pivotIndex, max);
 		int p = partition(arr, min, max);
 		swap(arr, p, max);
-		if ((p-min) > 1 && min < k) quicksort(arr, min, p-1, k);
-		if ((max-p) > 1 && p < k) quicksort(arr, p+1, max, k);
+		if (p == k-1) return arr[k-1];
+		else if ((p-min) > 1 && k   < p+1) return findSmallestK(arr, min, p-1, k);
+		else if ((max-p) > 1 && p+1 < k  ) return findSmallestK(arr, p+1, max, k);
+		return arr[k-1];
 	}
 
 	static void swap(Comparable[] arr, int p1, int p2) {
@@ -56,5 +55,17 @@ public class SearchSmallestK {
 			if (right > left) swap(arr, left, right);
 		}
 		return left;
+	}
+	
+	static void quicksort(Comparable[] arr) {
+		quicksort(arr, 0, arr.length-1);
+	}
+	static void quicksort(Comparable[] arr, int min, int max) {
+		int pivotIndex = (min+max)/2;
+		swap(arr, pivotIndex, max);
+		int p = partition(arr, min, max);
+		swap(arr, p, max);
+		if ((p-min) > 1) quicksort(arr, min, p-1);
+		if ((max-p) > 1) quicksort(arr, p+1, max);
 	}
 }
