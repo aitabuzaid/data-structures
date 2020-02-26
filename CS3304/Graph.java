702e6b63bc6160f941f455967be4ead065dce716
aitabuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2020-02-26 19:10:12 +0300

    Completed the graph class

--- CS3304/Graph.java
+++ CS3304/Graph.java
@@ -1,5 +1,37 @@
 package CS3304;
 
+import java.util.LinkedList;
+
 public class Graph {
+	private final int V;
+	private int E;
+	private LinkedList<Integer>[] adj;
+
+	public Graph(int V) {
+		this.V = V;
+		this.E = 0;
+		adj = new LinkedList[V];
+		for (int v = 0; v < V; v++) {
+			adj[v] = new LinkedList();
+		}
+	}
+	
+	public int V() {
+		return V;
+	}
+	
+	public int E() {
+		return E;
+	}
+	
+	public void addEdge(int v, int w) {
+		E++;
+		adj[v].add(w);
+		adj[w].add(v);
+	}
+	
+	public Iterable<Integer> adj(int v) {
+		return adj[v];
+	}
 
 }
