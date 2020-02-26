bc082a466d32ffc55fe0b8e870edd37008980c45
aitabuzaid <abdulrahman.abuzaid@kaust.edu.sa>
2020-02-26 20:07:15 +0300

    Completed the PrimMST class

--- CS3304/PrimMST.java
+++ CS3304/PrimMST.java
@@ -1,9 +1,38 @@
 package CS3304;
 
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class PrimMST {
 	
 	private boolean[] marked;
-	private Queue<Edge>
+	private Queue<Edge> mst;
+	private PriorityQueue<Edge> pq;
+	
+	public PrimMST(Graph G) {
+		pq = new PriorityQueue<Edge>();
+		mst = new LinkedList<Edge>();
+		marked = new boolean[G.V()];
+		visit(G, 0);
+		
+		while(!pq.isEmpty() && mst.size() < G.V()-1){
+			Edge e = pq.remove();
+			int v = e.eitherV();
+			int w = e.otherV(v);
+			if (marked[v] && marked[w]) continue;
+			mst.add(e);
+			if(!marked[v]) visit(G,v);
+			if(!marked[w]) visit(G,w);
+		}
+	}
+	
+	private void visit(Graph G, int v) {
+		marked[v] = true;
+		for (Edge e : G.adj(v))
+			if (!marked[e.otherV(v)])
+				pq.add(e);
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
